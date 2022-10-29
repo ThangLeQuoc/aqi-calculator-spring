7e94bcb060cfb569ba646900f4d2943f1014ba40
Thang Le Quoc <thanglequoc.it@gmail.com>
2022-10-29 18:24:07 +0700

    #1: Add base Dockerfile
    - Add the base Dockerfile, base image is openjdk:18.0.2-jdk-buster
    - Add sample test route

--- src/main/java/com/github/thanglequoc/aqicalculatorspringboot/AqiCalculatorSpringBootApplication.java
+++ src/main/java/com/github/thanglequoc/aqicalculatorspringboot/AqiCalculatorSpringBootApplication.java
@@ -2,9 +2,17 @@ package com.github.thanglequoc.aqicalculatorspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AqiCalculatorSpringBootApplication {
+
+	@GetMapping("/home")
+	public String home() {
+		return "Hello docker world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AqiCalculatorSpringBootApplication.class, args);
