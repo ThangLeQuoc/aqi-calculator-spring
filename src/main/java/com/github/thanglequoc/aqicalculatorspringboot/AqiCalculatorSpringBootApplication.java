554ce43cf1f511ea0ff1187916d2623e7d361105
Thang Le Quoc <thanglequoc.it@gmail.com>
2022-10-29 17:12:09 +0700

    Init commit

---
+++ src/main/java/com/github/thanglequoc/aqicalculatorspringboot/AqiCalculatorSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.thanglequoc.aqicalculatorspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqiCalculatorSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqiCalculatorSpringBootApplication.class, args);
+	}
+
+}
